f2190ac208f860e1204f10d6cca6883e4ff42461
HemanthM18 <mhemanth3131@gmail.com>
2024-02-22 12:53:06 +0530

    createdroutes.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ mongoose.connect(process.env.mongoURI,{
 });
 const db=mongoose.connection;
 
+const routes = require('./routes');
+
+app.use(express.json());
+app.use('/', routes);
+
 app.get('/',(req,res)=>{
   res.send(`${db.readyState===1?'Connected':'Disconnected'}`);
 });
