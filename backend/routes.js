9c735e9e27d921143e6160ea87226e47adfe4bb1
HemanthM18 <mhemanth3131@gmail.com>
2024-03-28 11:22:36 +0530

    Using component

--- backend/routes.js
+++ backend/routes.js
@@ -1,58 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-let brands = [
-    { id: 1, name: 'H&M', website:'https://www2.hm.com/en_in/index.html' },
-    { id: 2, name: 'HRX', website:'https://hrxbrand.com/home' }
-];
 
-// GET all brands
-router.get('/brands', (req, res) => {
-    res.json(brands);
+
+router.put('/put', async (req, res) => {
+    res.json({ Message: 'Data is updated.' });
 });
 
-// GET a single brand by ID
-router.get('/brands/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const brand = brands.find(brand => brand.id === id);
-    if (brand) {
-      res.json(brand);
-    } else {
-      res.status(404).json({ message: 'Brand not found' });
-    }
+router.post('/post', async (req, res) => {
+    res.json({ Message: 'Data is posted.' });
 });
 
-// Create (POST) a new user
-router.post('/brands', (req, res) => {
-    const { name, website } = req.body;
-    const newBrand = {
-        id: brands.length + 1, 
-        name : 'ZARA',
-        website: 'https://www.zara.com/in/'
-    };
-    brands.push(newBrand);
-    res.status(201).json(newBrand);
+router.delete('/delete', async (req, res) => {
+    res.json({ Message: 'Data is deleted.' });
 });
 
-// Update (PUT) an existing user
-router.put('/brands/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = brands.findIndex(brand => brand.id === id);
-    if (index === -1) {
-        return res.status(404).json({ message: 'Brand not found' });
-    }
-    const { name, website } = req.body;
-    brands[index].name = name;
-    brands[index].website = website;
-    res.json(brands[index]);
-});
-
-
-// Delete (DELETE) a user by ID
-router.delete('/brands/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    brands = brands.filter(brand => brand.id !== id);
-    res.json({ message: 'Brand deleted successfully' });
+router.get('/get', async (req, res) => {
+    res.json({ Message: 'Data is fetched.' });
 });
 
 module.exports = router;
--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,22 +1,36 @@
+// App.js
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Signup from './components/Signup';
-import Login from './components/Login';
+import './App.css'; 
+// import Data from './Components/Data';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function App() {
-  return(
+  let  [data, setData] = useState()
+
+  useEffect(()=>{
+    axios.get('http://localhost:8000/getBrands').then(res => setData(res.data)).catch(err => console.error(err))
+  })
+  return (
     <div>
-      <Routes>
-        <Route path="/" element={<About />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/Signup" element={<Signup />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-      </Routes>
+      <h1 className='welcome'>Welcome to BrandFactory</h1>
+      {/* <Data /> */}
+      <div>{data && data.map((item)=>{
+        return(
+          <div key={item._id} className="main">
+            <h2>{item.BrandName}</h2>
+            <h4>Description : {item.Description}</h4>
+            <h4>History : {item.History}</h4>
+            <h4>Founders : {item.Founders}</h4>
+            <h4>Mission Statement : {item.MissionStatement}</h4>
+            <h4>Selling Point : {item.SellingPoint}</h4>
+            <h4>Quality Standards : {item.QualityStandards}</h4>
+            <h4>Web Link : {item.WebLink}</h4>
+          </div>
+        )
+      })}</div>
     </div>
-  )
+  );
 }
 
 export default App;
