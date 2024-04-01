d2f6e9bc1dfd4cf8bde2c5a6a552ce4d20845556
HemanthM18 <mhemanth3131@gmail.com>
2024-04-01 11:45:38 +0530

    Using Components

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,78 @@
 const express = require('express');
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000;
-require('dotenv').config()
-console.log(process.env)
-
-const mongoose=require('mongoose');
-mongoose.connect(process.env.mongoURI,{
-  useNewUrlParser:true,
-  useUnifiedTopology:true,
-});
-const db=mongoose.connection;
-
+const port = 8000;
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
 const routes = require('./routes');
+const factoryModel = require('./Models/brands.js')
+require("dotenv").config();
+const uri = process.env.mongoURi;
+const mongoose = require('mongoose')
 
-app.use(express.json());
-app.use('/', routes);
 
-app.get('/',(req,res)=>{
-  res.send(`${db.readyState===1?'Connected':'Disconnected'}`);
+// Importing route handlers
+ // Assuming your routes are defined in a file named routes.js
+
+// MongoDB connection URL
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get('/ping', (req, res) => {
-  res.json({message: 'pong'});
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(uri,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
 })
+ 
+
+// app.use(express.json());
+
+// Use routes
+app.use('/', routes); 
+
+// app.get("/flavours", async (req, res) => {
+//   try {
+//     // Connect to the MongoDB database
+//     await client.connect(uri);
+
+//     // Check if the connection is successful
+//     if (client.topology.isConnected()) {
+//       res.json({ message: "pong", database_status: "Connected" });
+//       console.log("yes");
+//     } else {
+//       res.json({ message: "pong", database_status: "Disconnected" });
+//       console.log("no");
+//     }
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+// // define the ping route
+// app.get('/ping',(req,res)=>{
+//   res.send('pong');
+// });
+
+
+
+
+app.get('/getBrands',(req,res)=>{
+    factoryModel.find()
+  .then(brands => res.json(brands))
+  .catch(err => res.json(err))
+})
+
 
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`🚀 server running on PORT: ${port}`);
   });
 }
+
 
 module.exports = app;
